4e7731203ac46c4750684d62c15c3f01111f8a89
Garrett Honke <garretthonke@gmail.com>
2018-06-16 13:24:20 -0400

    update setup

---
+++ setupy.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(name='nk_unicorn',
+      version='1.0.0',
+      description='UNsupervised Image Clustering with Object Recognition Network system',
+      packages=['nk_croc'],
+      install_requires=[
+          'tensorflow == 1.8.0',
+          'Keras == 2.1.6',
+          'pandas >= 0.22.0, <= 0.23.0',
+          'tesserocr == 2.2.2',
+          'numpy >= 1.13.3',
+          'Pillow >= 5.1.0'],
+      )
