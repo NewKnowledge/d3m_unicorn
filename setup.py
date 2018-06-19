3c4d596481a3f446c0f1627af2fe95511050ed72
Garrett Honke <garretthonke@gmail.com>
2018-06-19 16:18:16 -0400

    fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='nk_unicorn',
       version='1.0.0',
       description='UNsupervised Image Clustering with Object Recognition Network system',
-      packages=['nk_croc'],
+      packages=['nk_unicorn'],
       install_requires=[
           'tensorflow == 1.8.0',
           'Keras == 2.1.6',
